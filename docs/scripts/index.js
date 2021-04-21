8088aa6190cc560f83f220bb211ce4c8abf015c5
JohnnySD <ba004629@bac.qld.edu.au>
2021-04-21 21:40:06 +1000

    Update

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -2,5 +2,5 @@ error = function() {
   alert("This page is currently under construction, sorry!");
 };
 mystery = function() {
-  alert("Pokémon PythonRed Mystery Gift #4: \"\"");
+  alert("Pokémon PythonRed Mystery Gift #4: \"OCEANIA\"");
 };
